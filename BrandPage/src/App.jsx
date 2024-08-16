9a303db35d46321bf1d4fe5b7b555f8db6ce279c
Rohan Shrestha <rohanxtha2060@gmail.com>
2024-08-16 23:13:20 +0545

    initial commit

---
+++ BrandPage/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Home from './components/home/Home'
+import Navigation from './components/navbar/Navigation'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <Navigation/>
+          <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ BrandPage/src/components/navbar/Navigation.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Navigation() {
+    return (
+    <>
+        <nav>
+            <div className='logo'>
+                <img src="./src/assets/nikelogo.png" alt="" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    </>
+    )
+}
